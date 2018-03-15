6c13afabb7a595d0db78c5af492500757b68f590
Ed Chapin <ed.chapin@nrc-cnrc.gc.ca>
2018-03-15 14:05:08 -0700

    scripts: update genIcd,py to optionally skip commands/events matching string

--- script/genIcd.py
+++ script/genIcd.py
@@ -14,11 +14,18 @@ from pyhocon import ConfigFactory
 ## main
 #
 
-if len(sys.argv) != 4:
+if (len(sys.argv) < 4) or (len(sys.argv) > 5):
     print """genIcd: Generate command-model.conf that sends all commands, and subscribe-model.conf that subscribes to all events, for assemblies under the input model path.
 
 usage:
-    genIcd <input model path> <output command-model file> <output subscribe-model file>
+    genIcd <input model path> <output command-model file> <output subscribe-model file> [skip description string]
+
+Note that the optional [skip description string] is a case-insensitve pattern match to the first line of the description for commands and events. Also, this string is going into a regex, so be careful to escape special characters
+
+example:
+The following would send all NFIRAOS commands, and subscribe to all NFIRAOS events, with the exception of any commands/events with (engineering) appearing in the first line
+
+    genIcd ICD-Model-Files/NFIRAOS-Model-Files command-model.conf subscribe-model.conf "\(engineering\)"
 """
     sys.exit(1)
 
@@ -26,13 +33,18 @@ in_dir = sys.argv[1]
 cmd_name = sys.argv[2]
 sub_name = sys.argv[3]
 
+if len(sys.argv) == 5:
+    skipstr = sys.argv[4]
+else:
+    skipstr = None
+
 # use a dictionary to keep track of differences between parsing
 # commands and events
 types = {}
-types['cmd'] = {
+types['  cmd'] = {
     'outname':cmd_name,            # name of the output file
-    'startstr': 'send = [\n',        # start file with this
-    'endstr'  : ']\n',               # end file with this
+    'startstr': 'send = [\n',      # start file with this
+    'endstr'  : ']\n',             # end file with this
     'space'   : '  ',              # space at start of each output line
     'inname':'command-model.conf', # name of model file to parse
     'key':'receive'                # key pointing to array to iterate over
@@ -85,6 +97,13 @@ for root, dirs, files in os.walk(in_dir):
                 outfile.write('\n%s// %s %s\n' % (spc,subsystem,component))
                 for r in a:
                     name = r['name']
+                    if skipstr and 'description' in r:
+                        if re.search("\A.*"+skipstr,r['description'],
+                                     re.IGNORECASE):
+                            print type, subsystem, component, name, \
+                                '[***SKIPPED***]'
+                            continue
+
                     print type, subsystem, component, name
                     s = """%s{
 %s  subsystem  = %s
